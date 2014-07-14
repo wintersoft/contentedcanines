30ceea8faf295ba3267828165ff1d463324b71b1
John Winter <john@wintersoft.co.nz>
2014-07-14 21:42:26 +1200

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CSS Portal - Layout</title>
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<link rel="stylesheet" type="text/css" href="css/base.css" />
+</head>
+<body>
+    <div id="wrapper">
+    	<div id="header">
+    	</div>
+    
+    	<div id="navigation">
+	    	<ul>
+	    		<li class=active><a href="index.php">Home</a></li>
+	    		<li><a href="about.php">About</a></li>
+	    		<li><a href="pricing.php">Pricing</a></li>
+	    		<li><a href="contact.php">Contact</a></li>
+	    	</ul>
+    	</div>
+    	
+    	<div id="walk-img"></div>
+    	
+        <div id="content">
+        	<h2>Contented Canines</h2>
+        	
+        	<img class="dog1" src="images/dog1.png">
+        	<img class="dog2" src="images/dog2.png">
+        	
+			<p>The name says it all.  A properly exercised and trained dog will be calm, contented and healthy. It is often difficult for dog owners to achieve the level of attention and exercise required for their dogs to be truly fit and healthy and mentally fulfilled in todays busy urban environment.</p>
+			
+			<p>Whether you'd like one day a week off the time consuming task of exercising or training your dog, regular walks, or would simply like your dog to receive more attention than you currently have time for, Contented Canines is the perfect solution. 'Short notice' casual walks and care can usually be catered for during unforeseen circumstances when you aren't able to exercise or care for your dog.</p>
+			
+			<p>Dog walks or runs will be individually tailored to suit each dog's particular requirements as to the level of activity and exercise you would like for them.   Basic training and playtime can be incorporated into the time spent with your dog if desired.</p>
+			</p>
+       </div>
+       
+       <div id="footer">Copyright &copy; 2014 - Contented Canines. All Rights Reserved.</div>
+    </div>
+</body>
+</html>

7dfbf925dca46e4534e668bf11bc8c52a0a01977
John Winter <john@wintersoft.co.nz>
2014-07-15 02:38:28 +1200

    Updated site

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         	<img class="dog1" src="images/dog1.png">
         	<img class="dog2" src="images/dog2.png">
         	
-			<p>The name says it all.  A properly exercised and trained dog will be calm, contented and healthy. It is often difficult for dog owners to achieve the level of attention and exercise required for their dogs to be truly fit and healthy and mentally fulfilled in todays busy urban environment.</p>
+			<p>The name says it all. Test.  A properly exercised and trained dog will be calm, contented and healthy. It is often difficult for dog owners to achieve the level of attention and exercise required for their dogs to be truly fit and healthy and mentally fulfilled in todays busy urban environment.</p>
 			
 			<p>Whether you'd like one day a week off the time consuming task of exercising or training your dog, regular walks, or would simply like your dog to receive more attention than you currently have time for, Contented Canines is the perfect solution. 'Short notice' casual walks and care can usually be catered for during unforeseen circumstances when you aren't able to exercise or care for your dog.</p>
 			

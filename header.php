60bbf2f513d04ae0931dcf1af8e1d2e462b260b6
John Winter <john@wintersoft.co.nz>
2014-07-23 06:20:33 +1200

    Updated site

--- header.php
+++ header.php
@@ -21,6 +21,16 @@ date_default_timezone_set('Pacific/Auckland');
 <link rel="stylesheet" type="text/css" href="<?=$prepend_path ?>css/tables.css" />
 </head>
 <body>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-53088629-1', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
     <div id="wrapper">
     	
     	<a href="<?=$site_url ?>"><div id="header"></div></a>

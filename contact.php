0f0bb63f244d65f51ca7c5ac553335fe0e9a4f87
John Winter <john@wintersoft.co.nz>
2014-07-23 06:01:43 +1200

    Updated site

--- contact.php
+++ contact.php
@@ -5,6 +5,17 @@
         <div id="content">
         	<h2>Contact</h2>
         	
+        	<img class="dog13" src="images/dog13.png">
+        	
+			Email: <a href='ma&#105;l&#116;o&#58;&#37;&#54;&#51;&#111;&#37;6E&#116;&#97;%&#54;3t%&#52;0c%6Fn&#116;e&#110;%74edc%&#54;1&#110;ine&#115;&#46;co&#46;%&#54;&#69;%7A'>c&#111;ntact&#64;con&#116;&#101;nt&#101;dc&#97;n&#105;nes&#46;c&#111;&#46;&#110;z</a>
+			<br><br>
+			<p>Cellphone: 027 303 5995</p>
+			<p>Phone and leave a message anytime, or text.</p>
+			<p>Email, text and phone enquiries will be responded to the same day.</p>
+			<br><br>
+			<h3>What happens next?</h3>
+			
+			<p>I will contact you and arrange a time for a free, no obligation meeting to discuss your requirements and meet your dog and/or pets.</p>
        </div>
        
 <?php include("footer.php") ?>

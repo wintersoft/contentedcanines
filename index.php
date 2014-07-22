529893526c4a145f711cb14c04b818d46f7aa252
John Winter <john@wintersoft.co.nz>
2014-07-23 04:29:18 +1200

    Updated site

--- index.php
+++ index.php
@@ -10,11 +10,15 @@
         	<img class="dog1" src="images/dog1.png">
         	<img class="dog2" src="images/dog2.png">
         	
+        	<img class="dog3" src="images/dog3.png">
+        	<img class="dog4" src="images/dog4.png">
+        	<img class="dog5" src="images/dog5.png">
+        	
 			<p>In today's busy urban environment it is difficult for many dog owners to achieve the level of exercise, attention and training required for our dogs to be truly fit, healthy and contented.</p>
 			
 			<p>Whether you would like one day off the task of exercising and training your dog, or long term regular walks, Contented Canines is the perfect solution.  Walks or runs will be individually matched to your dog's particular requirements and can include basic training and play if desired.</p>
 			</p>
-			
+			<br>
 			<h2>Also Available</h2>
 			
 			<p>Pet Care services by arrangement</p>
